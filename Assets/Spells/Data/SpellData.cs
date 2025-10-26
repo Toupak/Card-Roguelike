2e277cdf70addde34a2577ae28380a863ef3bd7d
richarx <theorichard12@gmail.com>
2025-10-26 17:08:36 +0100

    Create Spell Data

---
+++ Assets/Spells/Data/SpellData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Spells.Data
+{
+    public enum TargetType
+    {
+        Ally,
+        Enemy,
+        Self,
+        None
+    }
+
+    public enum TargetingMode
+    {
+        Single,
+        Multi,
+        All
+    }
+    
+    [CreateAssetMenu(fileName = "SpellData", menuName = "Scriptable Objects/SpellData")]
+    public class SpellData : ScriptableObject
+    {
+        public string spellName;
+        public string description;
+        public Sprite icon;
+        public TargetType targetType;
+        public TargetingMode targetingMode;
+        [Tooltip("Optional : only needed when TargetingMode is set to Multi")] public int targetCount = 1;
+    }
+}
