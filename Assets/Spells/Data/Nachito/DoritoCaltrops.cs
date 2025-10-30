6a0343f2cdd2fed842d1924a5a96cf8680ac762d
richarx <theorichard12@gmail.com>
2025-10-30 21:06:11 +0100

    Finish Nachito's spells

--- Assets/Spells/Data/Nachito/DoritoCaltrops.cs
+++ Assets/Spells/Data/Nachito/DoritoCaltrops.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using ActionReaction;
+using ActionReaction.Game_Actions;
 using Cards.Scripts;
 using UnityEngine;
 
@@ -8,6 +11,7 @@ namespace Spells.Data.Nachito
     public class DoritoCaltrops : SpellController
     {
         [SerializeField] private int stacksAppliedCount;
+        [SerializeField] private int damageOnPerformAction;
         
         public Dictionary<CardController, int> stacksDictionary { get; private set; } = new Dictionary<CardController, int>();
         public bool HasTargets => stacksDictionary.Count > 0;
@@ -37,19 +41,29 @@ namespace Spells.Data.Nachito
 
         private void OnEnable()
         {
-            //ActionSystem.SubscribeReaction<CardPerformActionGA>(CardPerformActionReaction, ReactionTiming.POST);
+            ActionSystem.SubscribeReaction<EnemyPerformsActionGa>(EnemyPerformsActionReaction, ReactionTiming.POST);
+            ActionSystem.SubscribeReaction<EndTurnGA>(EndTurnReaction, ReactionTiming.PRE);
         }
 
         private void OnDisable()
         {
-            //ActionSystem.UnsubscribeReaction<CardPerformActionGA>(CardPerformActionReaction, ReactionTiming.POST);
+            ActionSystem.UnsubscribeReaction<EnemyPerformsActionGa>(EnemyPerformsActionReaction, ReactionTiming.POST);
+            ActionSystem.UnsubscribeReaction<EndTurnGA>(EndTurnReaction, ReactionTiming.PRE);
         }
         
-        /*
-        public void StartTurnReaction(CardPerformActionGA cardPerformActionGA)
+        public void EnemyPerformsActionReaction(EnemyPerformsActionGa enemyPerformsActionGa)
         {
-            //cardPerformActionGA.controller.cardHealth.TakeDamage();
+            if (stacksDictionary.ContainsKey(enemyPerformsActionGa.cardController))
+                enemyPerformsActionGa.cardController.cardHealth.TakeDamage(damageOnPerformAction);
         }
-        */
+        
+        public void EndTurnReaction(EndTurnGA endTurnGa)
+        {
+            if (endTurnGa.ending == CombatLoop.CombatLoop.TurnType.Enemy)
+            {
+                stacksDictionary = stacksDictionary.Where(pair => pair.Value > 1)
+                    .ToDictionary(pair => pair.Key, pair => pair.Value - 1);
+            }
+        }
     }
 }
