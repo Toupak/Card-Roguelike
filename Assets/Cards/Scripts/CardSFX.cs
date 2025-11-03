802b2dca034ea38f0abe73cdfb1a42950d9851d5
Toupak <vitello.th@gmail.com>
2025-11-03 21:26:57 +0100

    Pickup/Drop sounds (InProgress)

---
+++ Assets/Cards/Scripts/CardSFX.cs
@@ -0,0 +1,36 @@
+using BoomLib.SFX_Player.Scripts;
+using Cards.Scripts;
+using System;
+using UnityEngine;
+
+public class CardSFX : MonoBehaviour
+{
+    [SerializeField] private AudioClip pickUp;
+    [SerializeField] private AudioClip drop;
+
+    private CardMovement cardMovement;
+
+    private void OnEnable()
+    {
+        cardMovement = GetComponent<CardMovement>();
+
+        cardMovement.OnStartDrag.AddListener(PlayPickUpSound);
+        cardMovement.OnDrop.AddListener(PlayDropSound);
+    }
+
+    private void OnDisable()
+    {
+        cardMovement.OnStartDrag.RemoveListener(PlayPickUpSound);
+        cardMovement.OnDrop.RemoveListener(PlayDropSound);
+    }
+
+    private void PlayPickUpSound()
+    {
+        SFXPlayer.instance.PlaySFX(pickUp);
+    }
+
+    private void PlayDropSound()
+    {
+        SFXPlayer.instance.PlaySFX(drop);
+    }
+}
