461aee5506d7e2d6ecacce1eca2355a86bdde9aa
Toupak <vitello.th@gmail.com>
2025-11-04 18:44:20 +0100

    MultiHit Behaviour

---
+++ Assets/EnemyAttack/Behaviours/MultiHitBehaviour.cs
@@ -0,0 +1,38 @@
+using ActionReaction;
+using ActionReaction.Game_Actions;
+using Cards.Scripts;
+using EnemyAttack;
+using System.Collections;
+using UnityEngine;
+
+public class MultiHitBehaviour : BaseEnemyBehaviour
+{
+    [SerializeField] private int damage;
+    [SerializeField] private int hitCount;
+    [SerializeField] private bool hitSameTarget;
+
+    public override IEnumerator ExecuteBehavior()
+    {
+        Debug.Log("Deal Damage Behaviour");
+
+        if (hitSameTarget)
+        {
+            CardController target = ComputeTarget(true);
+            for (int i = 0; i < hitCount; i++)
+            {
+                yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
+                DealDamageGA damageGa = new DealDamageGA(ComputeCurrentDamage(damage), enemyCardController.cardController, target);
+                ActionSystem.instance.Perform(damageGa);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < hitCount; i++)
+            {
+                yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
+                DealDamageGA damageGa = new DealDamageGA(ComputeCurrentDamage(damage), enemyCardController.cardController, ComputeTarget(true));
+                ActionSystem.instance.Perform(damageGa);
+            }
+        }
+    }
+}
