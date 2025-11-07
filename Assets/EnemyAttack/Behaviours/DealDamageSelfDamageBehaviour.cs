752b58ba6faf847a46cbf02d963401536823240b
Toupak <vitello.th@gmail.com>
2025-11-07 02:42:38 +0100

    Changed to correct order deal damage then self damage behaviour

--- Assets/EnemyAttack/Behaviours/DealDamageSelfDamageBehaviour.cs
+++ Assets/EnemyAttack/Behaviours/DealDamageSelfDamageBehaviour.cs
@@ -12,15 +12,15 @@ public class DealDamageSelfDamageBehaviour : BaseEnemyBehaviour
     public override IEnumerator ExecuteBehavior()
     {
         Debug.Log("Deal Damage Behaviour");
-        
-        yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
-
-        DealDamageGA selfDamageGa = new DealDamageGA(selfDamage, enemyCardController.cardController, enemyCardController.cardController);
-        ActionSystem.instance.Perform(selfDamageGa);
 
         yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
 
         DealDamageGA damageGa = new DealDamageGA(ComputeCurrentDamage(damage), enemyCardController.cardController, ComputeTarget(true));
         ActionSystem.instance.Perform(damageGa);
+
+        yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
+
+        DealDamageGA selfDamageGa = new DealDamageGA(selfDamage, enemyCardController.cardController, enemyCardController.cardController);
+        ActionSystem.instance.Perform(selfDamageGa);
     }
 }
