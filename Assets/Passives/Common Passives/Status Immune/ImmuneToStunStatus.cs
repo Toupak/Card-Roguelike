935d82639d012cbc3b77e1dca6482d7ef39ca226
richarx <theorichard12@gmail.com>
2025-11-08 19:40:49 +0100

    Added Frien

---
+++ Assets/Passives/Common Passives/Status Immune/ImmuneToStunStatus.cs
@@ -0,0 +1,28 @@
+using ActionReaction;
+using ActionReaction.Game_Actions;
+using Cards.Scripts;
+using UnityEngine;
+
+namespace Passives.Common_Passives.Status_Immune
+{
+    public class ImmuneToStunStatus : PassiveController
+    {
+        [SerializeField] private StatusType statusType;
+        
+        private void OnEnable()
+        {
+            ActionSystem.SubscribeReaction<ApplyStatusGa>(ImmuneToStatusReaction, ReactionTiming.PRE);
+        }
+
+        private void OnDisable()
+        {
+            ActionSystem.UnsubscribeReaction<ApplyStatusGa>(ImmuneToStatusReaction, ReactionTiming.PRE);
+        }
+        
+        private void ImmuneToStatusReaction(ApplyStatusGa applyStatusGa)
+        {
+            if (applyStatusGa.target == cardController && applyStatusGa.type == statusType)
+                applyStatusGa.NegateEffect();
+        }    
+    }
+}
---
+++ Assets/Spells/Data/Frien/FrienWeakBuff.cs
@@ -0,0 +1,29 @@
+using ActionReaction;
+using ActionReaction.Game_Actions;
+using Cards.Scripts;
+using Passives;
+
+namespace Spells.Data.Frien
+{
+    public class FrienWeakBuff : PassiveController
+    {
+        private void OnEnable()
+        {
+            ActionSystem.SubscribeReaction<DealDamageGA>(BuffDamageReaction, ReactionTiming.PRE);
+        }
+
+        private void OnDisable()
+        {
+            ActionSystem.SubscribeReaction<DealDamageGA>(BuffDamageReaction, ReactionTiming.PRE);
+        }
+
+        private void BuffDamageReaction(DealDamageGA dealDamageGa)
+        {
+            if (dealDamageGa.attacker == cardController && cardController.cardStatus.IsStatusApplied(StatusType.Weak))
+            {
+                int damageBonus = cardController.cardStatus.currentStacks[StatusType.Weak] * 2;
+                dealDamageGa.amount += damageBonus;
+            }
+        }
+    }
+}
