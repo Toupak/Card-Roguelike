6ee2cabb4323e588308fd207e6c4e07dacd7a57e
richarx <theorichard12@gmail.com>
2025-11-13 15:15:24 +0100

    Make “Call for Backup” a passive

--- Assets/EnemyAttack/Goboking Fight/Goboking/CallForBackup.cs
+++ Assets/EnemyAttack/Goboking Fight/Goboking/CallForBackup.cs
@@ -1,45 +1,55 @@
-using System.Collections;
+using System;
+using System.Collections.Generic;
 using ActionReaction;
-using EnemyAttack.Behaviours;
+using ActionReaction.Game_Actions;
+using Cards.Scripts;
+using Passives;
 using Spells;
 using Spells.Targeting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace EnemyAttack.Goboking_Fight.Goboking
 {
-    public class CallForBackup : SpawnEnemyBehaviour
+    public class CallForBackup : PassiveController
     {
-        [SerializeField] private int goblinsToSpawn;
-        [SerializeField] BaseEnemyBehaviour waitingBehaviourAfterSpawn;
-        [SerializeField] BaseEnemyBehaviour kingStompBehaviour;
+        [SerializeField] protected List<CardData> cardsToSpawn;
+        [SerializeField] private int goblinsToSpawnCount;
+        [SerializeField] private int turnsBetweenSpawns;
+        [SerializeField] private int maxGoblinCount;
 
         private int currentEnemyCount => TargetingSystem.instance.RetrieveBoard(TargetType.Enemy).Count;
-        
-        public override IEnumerator ExecuteBehavior()
+
+        private void OnEnable()
         {
-            yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
-
-            int spawnCount = goblinsToSpawn - (currentEnemyCount - 1);
-            
-            for (int i = 0;  i < spawnCount; i++)
-            {
-                yield return SpawnRandomEnemy();
-                yield return new WaitForSeconds(0.15f);
-            }
-
-            enemyCardController.SetNewIntention(waitingBehaviourAfterSpawn, true);
+            ActionSystem.SubscribeReaction<StartTurnGa>(StartTurnReaction, ReactionTiming.POST);
         }
 
-        public override int ComputeWeight()
+        private void OnDisable()
         {
-            if (currentEnemyCount <= 1)
-            {
-                if (CombatLoop.CombatLoop.instance.currentTurn != CombatLoop.CombatLoop.TurnType.Preparation)
-                    enemyCardController.SetNewIntention(waitingBehaviourAfterSpawn, true);
-                return weight;
-            }
+            ActionSystem.UnsubscribeReaction<StartTurnGa>(StartTurnReaction, ReactionTiming.POST);
+        }
 
-            return 0;
+        private void StartTurnReaction(StartTurnGa startTurnGa)
+        {
+            if (startTurnGa.starting != CombatLoop.CombatLoop.TurnType.Enemy)
+                return;
+
+            bool isSpawnTurn = CombatLoop.CombatLoop.instance.turnCount % turnsBetweenSpawns == 0;
+            if (isSpawnTurn && currentEnemyCount < maxGoblinCount)
+            {
+                int newGoblinCount = Mathf.Min(goblinsToSpawnCount, maxGoblinCount - currentEnemyCount);
+                for (int i = 0; i < newGoblinCount; i++)
+                {
+                    SpawnRandomEnemy();
+                }
+            }
+        }
+
+        private void SpawnRandomEnemy()
+        {
+            SpawnCardGA spawnCardGa = new SpawnCardGA(cardsToSpawn[Random.Range(0, cardsToSpawn.Count)], cardController);
+            ActionSystem.instance.AddReaction(spawnCardGa);
         }
     }
 }
--- Assets/EnemyAttack/Goboking Fight/Goboking/GoboWait.cs
+++ Assets/EnemyAttack/Goboking Fight/Goboking/GoboWait.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using ActionReaction;
-using Spells;
-using Spells.Targeting;
 using UnityEngine;
 
 namespace EnemyAttack.Goboking_Fight.Goboking
 {
     public class GoboWait : BaseEnemyBehaviour
     {
-        private int currentEnemyCount => TargetingSystem.instance.RetrieveBoard(TargetType.Enemy).Count;
-        
         public override IEnumerator ExecuteBehavior()
         {
             yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
@@ -17,9 +13,6 @@ namespace EnemyAttack.Goboking_Fight.Goboking
         
         public override int ComputeWeight()
         {
-            if (currentEnemyCount <= 1)
-                return 0;
-
             return weight;
         }
     }
