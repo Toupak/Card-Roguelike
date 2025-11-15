86a7aa6b9992c6660adbdb31fbcfe8f4751eb2b0
richarx <theorichard12@gmail.com>
2025-11-15 14:55:32 +0100

    Added Shuffle function in Tools

--- Assets/BoomLib/Tools/Tools.cs
+++ Assets/BoomLib/Tools/Tools.cs
@@ -201,5 +201,15 @@ namespace BoomLib.Tools
 
             return type;
         }
+        
+        public static void Shuffle<T>(this IList<T> ts) {
+            int count = ts.Count;
+            int last = count - 1;
+            
+            for (int i = 0; i < last; ++i) {
+                int r = Random.Range(i, count);
+                (ts[i], ts[r]) = (ts[r], ts[i]);
+            }
+        }
     }
 }
