641ae6eba91e35121b6aae2388a84b674eb9300a
richarx <theorichard12@gmail.com>
2025-11-16 07:11:44 +0100

    Combo/Bongo On Captured

--- Assets/Spells/Data/Bongo And Combo/BongoPassiveStatus.cs
+++ Assets/Spells/Data/Bongo And Combo/BongoPassiveStatus.cs
@@ -1,5 +1,6 @@
 using ActionReaction;
 using ActionReaction.Game_Actions;
+using Cards.Scripts;
 using Passives;
 
 namespace Spells.Data.Bongo_And_Combo
@@ -20,8 +21,9 @@ namespace Spells.Data.Bongo_And_Combo
         {
             bool targetIsNotMe = applyStatusGa.target != cardController;
             bool targetIsBrother = applyStatusGa.target != null && applyStatusGa.target.passiveHolder != null && applyStatusGa.target.passiveHolder.GetPassive(passiveData) != null;
+            bool isStatusCaptured = applyStatusGa.type == StatusType.Captured;
             
-            if (targetIsNotMe && targetIsBrother && !applyStatusGa.isBongoStatus)
+            if (targetIsNotMe && targetIsBrother && !applyStatusGa.isBongoStatus && !isStatusCaptured)
             {
                 ApplyStatusGa status = new ApplyStatusGa(applyStatusGa.type, applyStatusGa.amount, cardController, cardController);
                 status.isBongoStatus = true;
