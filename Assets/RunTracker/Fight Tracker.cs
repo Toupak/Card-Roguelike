f9f6bc0ff7ffb7fd26b03a89c1f4c0d652a9b32d
Toupak <vitello.th@gmail.com>
2025-11-29 17:16:55 +0100

    Started working on fight stat tracker

---
+++ Assets/RunTracker/Fight Tracker.cs
@@ -0,0 +1,73 @@
+using ActionReaction;
+using ActionReaction.Game_Actions;
+using Cards.Scripts;
+using Unity.VisualScripting;
+using UnityEngine;
+using static CombatLoop.CombatLoop;
+
+public class FightTracker : MonoBehaviour
+{
+    private int totalDMG;
+    private int avgDMGPerTurn;
+    private int thisTurnDMG;
+    private int lastTurnDMG;
+
+    private int turnCounter;
+    private int playerTurns;
+
+    private void OnEnable()
+    {
+        ActionSystem.SubscribeReaction<DealDamageGA>(DamageTrack, ReactionTiming.POST);
+        ActionSystem.SubscribeReaction<StartTurnGa>(UpdateTurn, ReactionTiming.PRE);
+    }
+
+    private void OnDisable()
+    {
+        ActionSystem.UnsubscribeReaction<DealDamageGA>(DamageTrack, ReactionTiming.POST);
+        ActionSystem.UnsubscribeReaction<StartTurnGa>(UpdateTurn, ReactionTiming.PRE);
+    }
+
+    void DamageTrack(DealDamageGA gA)
+    {
+        /*if (IsPlayerTurn() == false)
+            return;
+
+        if (ShouldNotBeCounted(gA))
+            return;
+        */
+        avgDMGPerTurn = (avgDMGPerTurn + gA.amount) / playerTurns;
+        thisTurnDMG += gA.amount;
+        totalDMG += gA.amount;
+
+        Debug.Log("TOUP Average DMG per turn : " + avgDMGPerTurn);
+        Debug.Log("TOUP This Turn DMG : " + thisTurnDMG);
+        Debug.Log("TOUP Last Turn DMG : " + lastTurnDMG);
+        Debug.Log("TOUP Total Damage : " + totalDMG);
+
+        //UpdateDisplay
+    }
+
+    void UpdateTurn(StartTurnGa gA)
+    {
+        turnCounter += 1;
+
+        if (gA.starting == TurnType.Player)
+        {
+            playerTurns++;
+            lastTurnDMG = thisTurnDMG;
+            thisTurnDMG = 0;
+            //UpdateDisplay;
+        }
+        else
+            return;
+    }
+
+    /*private bool ShouldNotBeCounted(DealDamageGA gA)
+    {
+        if (gA.target.enemyCardController == null && IsPlayerTurn() == true)
+            return true;
+        else
+            return true;
+    }
+    */
+}
