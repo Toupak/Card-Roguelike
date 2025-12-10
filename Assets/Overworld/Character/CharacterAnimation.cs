41140e138c940aea4f88f73bcc2cab1b47d1abcf
Toupak <vitello.th@gmail.com>
2025-12-10 05:22:05 +0100

    Basic CharacterAnimations Walk/Idle with temporary test SpriteSheet for character

---
+++ Assets/Overworld/Character/CharacterAnimation.cs
@@ -0,0 +1,36 @@
+using BoomLib.Tools;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    [SerializeField] private CharacterMovement movement;
+    [SerializeField] private Rigidbody2D rb;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void LateUpdate()
+    {
+        if (rb.linearVelocity.magnitude > 0.1f)
+            PlayAnimation("Walk");
+        else
+            PlayAnimation("Idle");
+    }
+
+    private void PlayAnimation(string animation)
+    {
+        animator.Play($"{animation}_{GetLastDirection()}");
+    }
+
+    private string GetLastDirection()
+    {
+        string[] directions = { "Right", "Up", "Left", "Down" };
+
+        int direction = Tools.GetCardinalDirection(movement.lastMovement);
+
+        return directions[direction];
+    }
+}
