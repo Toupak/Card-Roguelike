c792b20e60b82a06a6d3dde1947c9e8eae315676
richarx <theorichard12@gmail.com>
2025-12-26 05:57:09 +0100

    Added Stats to SaveData

--- Assets/Save System/SaveData.cs
+++ Assets/Save System/SaveData.cs
@@ -5,6 +5,29 @@ namespace Save_System
     [Serializable]
     public class SaveData
     {
-        public int launchTime;
+
+        #region STATS
+        
+        public DateTime firstTimePlayed;
+        public DateTime lastTimePlayed;
+        public float totalPlayTimeInSeconds;
+        
+        public int totalRunCount;
+        public int totalRunWinCount;
+        
+        public int totalBattleCount;
+        public int totalKillCount;
+        public int totalDamageDealt;
+        public int totalDamageReceived;
+        public int highestDamageInOneAttack;
+        public int highestTokenCountInOneTurn;
+        
+        //public string mostPlayedCharacter;
+        //public string mostPlayedCard;
+        //public string cardWithBestWinRate;
+
+        public int totalBoosterOpenedCount;
+        
+        #endregion
     }
 }
