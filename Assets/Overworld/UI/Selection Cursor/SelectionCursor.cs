7c7c18a5a0da125946cf020561f2243ee3d4ad75
richarx <theorichard12@gmail.com>
2025-12-29 00:34:42 +0100

    Animate Selection Cursor

--- Assets/Overworld/UI/Selection Cursor/SelectionCursor.cs
+++ Assets/Overworld/UI/Selection Cursor/SelectionCursor.cs
@@ -1,3 +1,5 @@
+using System;
+using PrimeTween;
 using UnityEngine;
 
 namespace Overworld.UI.Selection_Cursor
@@ -9,12 +11,23 @@ namespace Overworld.UI.Selection_Cursor
         [SerializeField] private Transform botRight;
         [SerializeField] private Transform botLeft;
 
+        [Space] 
+        [SerializeField] private float endValue;
+        [SerializeField] private float duration;
+        [SerializeField] private Ease ease;
+        
         public void Setup(Vector2 size, Vector2 offset)
         {
             topLeft.localPosition = offset + new Vector2(-size.x, size.y);
             topRight.localPosition = offset + new Vector2(size.x, size.y);
             botRight.localPosition = offset + new Vector2(size.x, -size.y);
             botLeft.localPosition = offset + new Vector2(-size.x, -size.y);
+        }
+
+        private void Start()
+        {
+            Sequence.Create(cycles:-1, cycleMode: Sequence.SequenceCycleMode.Yoyo)
+                .Chain(Tween.Scale(transform, endValue, duration, ease));
         }
     }
 }
