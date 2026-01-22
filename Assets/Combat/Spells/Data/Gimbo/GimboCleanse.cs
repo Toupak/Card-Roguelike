81e75ecdcf2df0e96245724cc4c2176cf5cd6631
richarx <theorichard12@gmail.com>
2026-01-22 22:54:21 +0100

    Fix Gimbo spell

--- Assets/Combat/Spells/Data/Gimbo/GimboCleanse.cs
+++ Assets/Combat/Spells/Data/Gimbo/GimboCleanse.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using ActionReaction;
+using ActionReaction.Game_Actions;
 using Cards.Scripts;
 using Combat.EnergyBar;
 using UnityEngine;
@@ -31,6 +32,14 @@ namespace Combat.Spells.Data.Gimbo
         {
             yield return base.CastSpellOnTarget(targets);
             yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
+
+            ConsumeStacksGa consumeStacksGa = new ConsumeStacksGa(StatusType.Stun, cardController.cardStatus.GetCurrentStackCount(StatusType.Stun), cardController, cardController);
+            ActionSystem.instance.Perform(consumeStacksGa);
+            
+            yield return new WaitWhile(() => ActionSystem.instance.IsPerforming);
+
+            ApplyStatusGa applyStatusGa = new ApplyStatusGa(spellData.inflictStatus, spellData.statusStacksApplied, cardController, cardController);
+            ActionSystem.instance.Perform(applyStatusGa);
         }
     }
 }
